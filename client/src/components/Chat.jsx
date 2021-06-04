9ca19511ff36010826a63fe152c9016c1c506a54
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2021-06-04 21:35:33 +0400

    настройка socket.io, добавление юзера в комнату

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,10 +1,30 @@
 import { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+const ENDPOINT = 'http://localhost:3000';
+
+let socket;
 
 
-
-const Chat = () => {
+const Chat = ({location}) => {
+   // const [messages, setMessages] = useState([]);
    const [message, setMessage] = useState('');
+   const [name, setName] = useState('');
+   const [room, setRoom] = useState('');
 
+
+   useEffect(() => {
+      const { name, room } = queryString.parse(location.search);
+      socket = io(ENDPOINT, {
+       transports: ['websocket', 'polling', 'flashsocket'],
+         });
+
+      // setRoom(room);
+      // setName(name);
+
+      socket.emit('join', { name, room });
+   }, [location.search, ENDPOINT])
 
    const sendMessage = () => {
       console.log(message)
--- server/index.js
+++ server/index.js
@@ -4,6 +4,15 @@ const server = require('http').createServer(express);
 const socketio = require('socket.io');
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+  console.log(`connected user ${socket.id}`);
+
+  socket.on('join', ({ name, room }) => {
+    console.log(`name ${name} , room ${room}`);
+    socket.join(room);
+  });
+});
+
 const PORT = '3000';
 
 server.listen(PORT, () => {
